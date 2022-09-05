73081b8a177a951ebece48361c893ee8cc53e196
João Pedro <jpedroarag@hotmail.com>
2022-09-05 12:49:50 -0300

    Corrigindo conversões

--- auth/email.js
+++ auth/email.js
@@ -4,8 +4,8 @@ require("dotenv").config()
 exports.mailer = nodemailer.createTransport({
     host: process.env.MAIL_SERVICE_URL,
     service: process.env.MAIL_SERVICE_URL,
-    port: process.env.MAIL_SERVICE_PORT,
-    secure: process.env.MAIL_SERVICE_ISSECURE,
+    port: process.env.MAIL_SERVICE_PORT * 1,
+    secure: (process.env.MAIL_SERVICE_ISSECURE === 'true'),
     tls: {
         ciphers: process.env.MAIL_SERVICE_TLSCIPHERS
     },
